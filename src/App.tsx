bb19e057e793ff36341c8b9b065e8ff80f453c02
AnjoC07 <anjocaluya.ama@gmail.com>
2023-09-27 16:21:27 +0800

    MUI NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 // App.tsx
 import "./App.css";
-import { MuiImageList } from "./components/MuiImageList";
+import { MuiNavbar } from "./components/MuiNavbar";
 
+// import { MuiImageList } from "./components/MuiImageList";
 // import { MuiAccordion } from "./components/MuiAccordion";
 // import { MuiCard } from "./components/MuiCard";
 // import { MuiLayout } from "./components/MuiLayout";
@@ -30,7 +31,8 @@ function App() {
       {/* <MuiLayout /> */}
       {/* <MuiCard /> */}
       {/* <MuiAccordion /> */}
-      <MuiImageList />
+      {/* <MuiImageList /> */}
+      <MuiNavbar />
     </div>
   );
 }
---
+++ src/components/MuiNavbar.tsx
@@ -0,0 +1,31 @@
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
+
+export const MuiNavbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton size="large" edge="start" color="inherit" aria-label="logo">
+          <CatchingPokemonIcon />
+        </IconButton>
+        {/* APP TITLE */}
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          POKEMONAPP
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <Button color="inherit">Features</Button>
+          <Button color="inherit">Pricing</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Login</Button>
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
+};
