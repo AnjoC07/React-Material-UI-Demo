118566d67c72ad191cbf2cb540ad4ab954a50977
AnjoC07 <anjocaluya.ama@gmail.com>
2023-09-27 13:58:28 +0800

    MUI Layout - Box, Stack, Grid and Paper Components

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -2,10 +2,20 @@ import { Box, Stack, Grid, Paper } from "@mui/material";
 
 // Box components in MUI used as replacement in Div
 // Box accepts layout component prompt such as span
+
 export const MuiLayout = () => {
   return (
+    // Paper - Establishing Visual hierarchy - ex: signup button, card component
     <Paper sx={{ padding: "32px" }} elevation={2}>
+      {/* 2. Stack  component for one dimensional Layout */}
+      {/* <Stack 
+        sx={{ border: "1px solid" }} 
+        direction="row" 
+        spacing={2} 
+        divider={<Divider orientation="vertical" flexItem/>}
+      > */}
       <Stack border="1px solid" spacing={2} direction="row">
+        {/* 1. Box */}
         <Box
           component="span"
           sx={{
@@ -30,7 +40,11 @@ export const MuiLayout = () => {
           p={2}
         ></Box>
       </Stack>
+      {/* 3. Grid Component */}
+      Grid
+      {/* Grid Container for Parent */}
       <Grid rowSpacing={2} columnSpacing={1} container my={4}>
+        {/* Grid Container for Children */}
         <Grid item xs={6}>
           <Box p={2} bgcolor="primary.light">
             Item 1
